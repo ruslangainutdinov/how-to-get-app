862eef7fc1329eade3f1b1d65d0e31ea051eda18
Ruslan <Ruslan@DESKTOP-20NQDPN>
2020-07-12 13:11:24 +0600

    NavBar was refactored as Thymeleaf fragment

--- how-to-get/src/main/java/com/ruslanproject/howtoget/HowToGetApplication.java
+++ how-to-get/src/main/java/com/ruslanproject/howtoget/HowToGetApplication.java
@@ -2,16 +2,8 @@ package com.ruslanproject.howtoget;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.MessageSource;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import com.ruslanproject.howtoget.utils.ApplicationMappings;
-import com.ruslanproject.howtoget.utils.ApplicationViews;
 
 @SpringBootApplication
 @EnableScheduling
